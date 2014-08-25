382b14b2f9930ff27b88bc9b3af8d8a69591050a
Paul Fournel <paul@paul-Latitude-E5520.(none)>
2014-08-25 20:21:24 +0200

    added a consol user interface.

--- main.c
+++ main.c
@@ -5,14 +5,106 @@
 #include <pthread.h>
 #include <stdio.h>
 
-// J'ai ajouté un arrondi pour l'affichage des matrices
-// afin de simplifier la lecture.
+void user_input_test(){
+    double d;
+    int i,j;
+    Matrix *mat = Matrix_create(4,4);
+    for(i=0;i<4;i++){
+        for(j=i;j<4;j++){
+            printf("\nA(%d,%d) = ", i+1,j+1);
+            scanf("%lf", &d);
+            Matrix_set(mat,i,j,d);
+            Matrix_set(mat,j,i,d);
+        }
+    }
+    printf("\nMatrice A\n");
+    Matrix_dump(mat);
+
+    printf("\nWhat algorithm should we use?\n\n 1. Normal\n 2. Parallel\n 3. Return to main\n");
+    int choice=-1;
+    while(choice<1 || choice>3){
+        printf("\n>: ");
+        scanf("%d", &choice);
+        if(choice==1){
+            Matrix *m2 = cholesky1(mat);
+            printf("\nMatrice L\n");
+            Matrix_dump(m2);
+        }
+        if(choice==2){
+            Matrix *m2 = cholesky2Para(mat);
+            printf("\nMatrice L\n");
+            Matrix_dump(m2);
+        }
+        if(choice==3){
+            //do nothing
+        }
+    }
+}
+
+void random_matrix_test(){
+    // Construction d'une matrice SDP
+    printf("\nMatrice A created:\n");
+    Matrix *m1=Matrix_create_random_SDP(10);//Matrix_create_sample();
+    Matrix_dump(m1);
+
+
+    printf("\nWhat algorithm should we use?\n\n 1. Normal\n 2. Parallel\n 3. Return to main\n");
+    int choice=-1;
+    Matrix *m2;
+    while(choice<1 || choice>3){
+        printf("\n>: ");
+        scanf("%d", &choice);
+        if(choice==1){
+            m2 = cholesky1(m1);
+            printf("\nMatrix L\n");
+            Matrix_dump(m2);
+        }
+        if(choice==2){
+            m2 = cholesky2Para(m1);
+            printf("\nMatrix L\n");
+            Matrix_dump(m2);
+        }
+        if(choice==3){
+            //do nothing
+        }
+    }
+
+    // On reconstruit la matrice initiale pour tester le résultat.
+    Matrix *m3bis = Matrix_transpose(m2);
+    Matrix *m3 = Matrix_product(m2, m3bis);
+    printf("\nMatrix L.t(L)\n");
+    Matrix_dump(m3);
+    int same = Matrix_isSame(m1, m3, 0.01);
+    if(same==1){
+        printf("\n***  The algorithm worked   ***\n");
+    }else{
+        printf("An error has occured\n");
+    }
+}
 
 int main()
 {
+    // ask user what to do
+
+    int choice=-1;
+    while(choice!=3){
+        printf("\nWhat would you like to do?\n\n 1. Enter a 4*4 matrix\n 2. Test with a random matrix\n 3. Exit\n");
+        printf("\n>: ");
+        scanf("%d", &choice);
+        if(choice==1){
+            user_input_test();
+        }
+        if(choice==2){
+            random_matrix_test();
+        }
+    }
+
+/*
+//    fscanf (stdin, "%s", mychar);
+
     // Construction d'une matrice SDP
     printf("\nMatrice A\n");
-    Matrix *m1=Matrix_create_random_SDP(200);//Matrix_create_sample();
+    Matrix *m1=Matrix_create_random_SDP(10);//Matrix_create_sample();
     Matrix_dump(m1);
 
 
@@ -29,7 +121,6 @@ int main()
     int same = Matrix_isSame(m1, m3, 0.01);
     printf("\nres: %d!\n", same);
     printf("\nthis is how it's done!\n");
-
-
+*/
     return 0;
 }
